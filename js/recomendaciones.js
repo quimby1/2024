9b51a955732f61eee864c99ec1e6c0856e4af1d7
sofiaquimbay <sofiaquimbaycadena@gmail.com>
2024-01-08 15:07:58 -0500

    Prueba podcast

---
+++ js/recomendaciones.js
@@ -0,0 +1,9 @@
+const podcast = document.querySelector(".podcast")
+const libros = document.querySelector(".libros")
+const lugares = document.querySelector(".lugares")
+const ig = document.querySelector(".ig")
+
+
+podcast.addEventListener("click",() =>{
+    window.location.href = "spoty.html";
+})
