e7a9b77ade9a9515bab9a1cc3eb0619c25cc6eca
sofiaquimbay <sofiaquimbaycadena@gmail.com>
2024-01-08 17:22:02 -0500

    Recomendaciones

--- js/recomendaciones.js
+++ js/recomendaciones.js
@@ -2,8 +2,25 @@ const podcast = document.querySelector(".podcast")
 const libros = document.querySelector(".libros")
 const lugares = document.querySelector(".lugares")
 const ig = document.querySelector(".ig")
+const series = document.querySelector(".series")
 
 
 podcast.addEventListener("click",() =>{
     window.location.href = "spoty.html";
+})
+
+libros.addEventListener("click",() =>{
+    window.location.href = "libros.html";
+})
+
+lugares.addEventListener("click",() =>{
+    window.location.href = "lugares.html";
+})
+
+ig.addEventListener("click",() =>{
+    window.location.href = "ig.html";
+})
+
+series.addEventListener("click",() =>{
+    window.location.href = "series.html";
 })
