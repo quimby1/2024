334e46735b6fd251a053c0620adf8bf30482c5e4
sofiaquimbay <sofiaquimbaycadena@gmail.com>
2024-01-23 19:16:11 -0500

    Cambios libros

---
+++ js/libros.js
@@ -0,0 +1,8 @@
+var boton1 = document.getElementById("manifiesto");
+
+
+
+
+boton1.addEventListener("click", function() {
+    window.open("https://www.google.com/search?sca_esv=ff4875abe50469e9&rlz=1C1UUXU_esCO1086CO1086&sxsrf=ACQVn0-S3RnZZlfPOYPRTRNCem46uaIbeg:1706055026400&q=Manifiesto+para+los+heroes+de+cada+d%C3%ADa&tbm=shop&source=lnms&biw=1396&bih=663&dpr=1.38", "_blank");
+});
