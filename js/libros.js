b4d3b665847eb704257c6ec555b005242e2700af
sofiaquimbay <sofiaquimbaycadena@gmail.com>
2024-01-23 20:45:39 -0500

    Se agregan más libros

--- js/libros.js
+++ js/libros.js
@@ -5,6 +5,19 @@ var boton4 = document.getElementById("manifiestoA");
 var boton5 = document.getElementById("sinmiedoA");
 var boton6 = document.getElementById("cienciaA");
 
+var boton7 = document.getElementById("silocrees");
+var boton8 = document.getElementById("silocreesA");
+var boton9 = document.getElementById("buenascosas");
+var boton10 = document.getElementById("buenascosasA");
+var boton11 = document.getElementById("creeenti");
+var boton12 = document.getElementById("creeentiA");
+var boton13 = document.getElementById("mujeresque");
+var boton14 = document.getElementById("mujeresqueA");
+var boton15 = document.getElementById("milagro");
+var boton16 = document.getElementById("milagroA");
+
+
+
 boton1.addEventListener("click", function() {
     window.open("https://www.google.com/search?sca_esv=ff4875abe50469e9&rlz=1C1UUXU_esCO1086CO1086&sxsrf=ACQVn0-S3RnZZlfPOYPRTRNCem46uaIbeg:1706055026400&q=Manifiesto+para+los+heroes+de+cada+d%C3%ADa&tbm=shop&source=lnms&biw=1396&bih=663&dpr=1.38", "_blank");
 });
@@ -27,4 +40,45 @@ boton5.addEventListener("click", function() {
 
 boton6.addEventListener("click", function() {
     window.open("https://play.google.com/store/audiobooks/details/Napoleon_Hill_La_ciencia_del_%C3%A9xito?id=AQAAAEAClnPWYM");
+});
+
+boton7.addEventListener("click", function() {
+    window.open("https://www.panamericana.com.co/si-lo-crees-lo-creas-588231/p");
+});
+
+boton8.addEventListener("click", function() {
+    window.open("https://www.penguinlibros.com/co/tematicas/43981-audiolibro-si-lo-crees-lo-creas-9786073170949");
+});
+
+boton9.addEventListener("click", function() {
+    window.open("https://www.librerianacional.com/producto/como-hacer-que-te-pasen-cosas-buenas-402629?gad_source=1&gclid=CjwKCAiA5L2tBhBTEiwAdSxJX_7AaJo7pjQ9QSFYZxq-AC05VENIWCKVrYakBDzrMjirGfOhHvkmSBoCFEgQAvD_BwE");
+});
+
+boton10.addEventListener("click", function() {
+    window.open("https://www.audible.com/es_US/pd/Como-hacer-que-te-pasen-cosas-buenas-Audiolibro/846705591X?psafe_param=1&source_code=UHSPP30DTRIAL5471013220187&ds_rl=1261256&cvosrc=ppc%20content.googlegdn.18599912777&cvo_crid=628348997659&cvo_pid=140238285977&source_code=&acs_info=ZmluYWxfdXJsOiAiaHR0cHM6Ly93d3cuYXVkaWJsZS5jb20vcGQvQ29tby1oYWNlci1xdWUtdGUtcGFzZW4tY29zYXMtYnVlbmFzLUF1ZGlvYm9vay84NDY3MDU1OTFYIgo&gclid=CjwKCAiA5L2tBhBTEiwAdSxJX4EmnXeF1oFG9smqrUeH_1ojEvZG6bJdCyN-jzuCDFuXELES_KDj5RoCO44QAvD_BwE&gclsrc=aw.ds");
+});
+
+boton11.addEventListener("click", function() {
+    window.open("https://www.panamericana.com.co/cree-en-ti-545589/p");
+});
+
+boton12.addEventListener("click", function() {
+    window.open("https://www.audible.com/es_US/pd/Cree-en-ti-Audiolibro/8408235532?source_code=UHSPP30DTRIAL547101122017T&source_code=UHSPP30DTRIAL5471013220187&ds_rl=1261256&cvosrc=ppc%20content.googlegdn.18599912777&cvo_crid=628348997659&cvo_pid=140238285977&source_code=&acs_info=ZmluYWxfdXJsOiAiaHR0cHM6Ly93d3cuYXVkaWJsZS5jb20vcGQvQ3JlZS1lbi10aS1BdWRpb2Jvb2svODQwODIzNTUzMiIK&gclid=CjwKCAiA5L2tBhBTEiwAdSxJX36Ss1iQdBloyfsNPnw13gtD87RlpqGcT3BKFBjv9Q0JItjnk6QU_hoCW0wQAvD_BwE&gclsrc=aw.ds");
+});
+
+boton13.addEventListener("click", function() {
+    window.open("https://www.panamericana.com.co/las-mujeres-que-aman-demasiado-554806/p?idsku=554806&utm_source=google&utm_medium=cpa&utm_campaign=Co_Panamericana_google__conversion_tecnologia__aon_max_performance&utm_id=conversion&gad_source=1&gclid=CjwKCAiA5L2tBhBTEiwAdSxJXy2z6f7qCGuJJCuGlH8WWhQdWMzW9yc6cpg5MhUF8oAd3qXcBYtYaRoCj-wQAvD_BwE");
+});
+
+boton14.addEventListener("click", function() {
+    window.open("https://play.google.com/store/audiobooks/details/Robin_Norwood_Las_mujeres_que_aman_demasiado?id=AQAAAEDMyx6LwM");
+});
+
+
+boton15.addEventListener("click", function() {
+    window.open("https://www.librerianacional.com/producto/el-milagro-metabolico?gad_source=1&gclid=CjwKCAiA5L2tBhBTEiwAdSxJX7fvykHhuA75qFidPKACO5-KvsIzmyd-gnpCSfbDe2-_pRUwSlcDPRoC628QAvD_BwE");
+});
+
+boton16.addEventListener("click", function() {
+    window.open("https://www.audible.com/es_US/pd/El-milagro-metabolico-Audiolibro/9584290053?source_code=UHSPP30DTRIAL548101222017X&ipRedirectOverride=true&source_code=UHSPP30DTRIAL5471013220187&ds_rl=1261256&cvosrc=ppc%20content.googlegdn.18599912777&cvo_crid=628348051120&cvo_pid=140238285977&source_code=&acs_info=ZmluYWxfdXJsOiAiaHR0cHM6Ly93d3cuYXVkaWJsZS5jb20vcGQvRWwtbWlsYWdyby1tZXRhYm9saWNvLUF1ZGlvYm9vay85NTg0MjkwMDUzIgo&gclid=CjwKCAiA5L2tBhBTEiwAdSxJX2NNPPdcJFqNLl9xn3MjUn10ejR--3j6iUK2jXxESICw2PGzcXU-phoCpmkQAvD_BwE&gclsrc=aw.ds");
 });
