e1d2e78295a5cbf43c64bd6d115d170f154decfb
sofiaquimbay <sofiaquimbaycadena@gmail.com>
2024-01-23 19:59:02 -0500

    Responsive libros

--- js/libros.js
+++ js/libros.js
@@ -1,8 +1,30 @@
 var boton1 = document.getElementById("manifiesto");
-
-
-
+var boton2 = document.getElementById("sinmiedo");
+var boton3 = document.getElementById("ciencia");
+var boton4 = document.getElementById("manifiestoA");
+var boton5 = document.getElementById("sinmiedoA");
+var boton6 = document.getElementById("cienciaA");
 
 boton1.addEventListener("click", function() {
     window.open("https://www.google.com/search?sca_esv=ff4875abe50469e9&rlz=1C1UUXU_esCO1086CO1086&sxsrf=ACQVn0-S3RnZZlfPOYPRTRNCem46uaIbeg:1706055026400&q=Manifiesto+para+los+heroes+de+cada+d%C3%ADa&tbm=shop&source=lnms&biw=1396&bih=663&dpr=1.38", "_blank");
+});
+
+boton2.addEventListener("click", function() {
+    window.open("https://www.panamericana.com.co/sin-miedo-645586/p?idsku=645586&utm_source=google&utm_medium=cpa&utm_campaign=Co_Panamericana_google__conversion_tecnologia__aon_max_performance&utm_id=conversion&gad_source=4&gclid=CjwKCAiA5L2tBhBTEiwAdSxJX80-YkRx-UrSo_vsUzpTu4nItKRElC75NepztMXx3_QHloCG6UdpcBoCCvYQAvD_BwE", "_blank");
+});
+
+boton3.addEventListener("click", function() {
+    window.open("https://www.librerianacional.com/producto/la-ciencia-del-exito?gad_source=4&gclid=CjwKCAiA5L2tBhBTEiwAdSxJX9JzZ3d01lbewvdySb4fPiQXqdKOFDs3Iu0mRFex5VMTATlDLayxxhoCXZIQAvD_BwE", "_blank");
+});
+
+boton4.addEventListener("click", function() {
+    window.open("https://www.penguinlibros.com/co/tematicas/258246-audiolibro-manifiesto-para-los-heroes-de-cada-dia-9788425360190", "_blank");
+});
+
+boton5.addEventListener("click", function() {
+    window.open("https://play.google.com/store/audiobooks/details/Sin_miedo_El_m%C3%A9todo_comprobado_para_superar_la_ans?id=AQAAAEA8BHtEVM&hl=es_CO&gl=US");
+});
+
+boton6.addEventListener("click", function() {
+    window.open("https://play.google.com/store/audiobooks/details/Napoleon_Hill_La_ciencia_del_%C3%A9xito?id=AQAAAEAClnPWYM");
 });
